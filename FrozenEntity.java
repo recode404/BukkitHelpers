c8f974c1be75a0e695f04d8729c5de1c10e1efa8
Taylor T <recode404@users.noreply.github.com>
2017-01-07 03:38:08 -0500

    Add files via upload

---
+++ FrozenEntity.java
@@ -0,0 +1,36 @@
+import net.minecraft.server.v1_11_R1.EntityLiving;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+/**
+ * Many people have been struggling to spawn in entities with No AI (Frozen) with proper positions and rotations.
+ * This is the simplest solution I have found to fix the rotation problem.
+ */
+public class FrozenEntity {
+	
+	private EntityLiving living;
+	
+	//Create a frozen entity with an NMS entity. Use this code template to create entities:
+	// FrozenEntity frozen = new FrozenEntity(new EntityZombie((CraftWorld)Bukkit.getWorld("world")).getHandle());
+	public FrozenEntity(EntityLiving living) {
+		this.living = living;
+	}
+	
+	//spawn in the frozen entity at the given location
+	public Entity spawn(Location loc) {
+		living.h(loc.getYaw());
+		living.i(loc.getYaw());
+		living.setPositionRotation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+		living.getWorld().addEntity(living, CreatureSpawnEvent.SpawnReason.CUSTOM);
+		((LivingEntity)living.getBukkitEntity()).setAI(false);
+		
+		return living.getBukkitEntity();
+	}
+	
+	public Entity getEntity() {
+		return living.getBukkitEntity();
+	}
+	
+}
---
+++ ItemFormatter.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+/**
+ * Class for teaching how to format Item Stacks.
+ * Also can be used as a helper class when formatting Item Stacks.
+ */
+public class ItemFormatter {
+	
+	private ItemStack item;
+	
+	public ItemFormatter(ItemStack item) {
+		this.item = item;
+	}
+	
+	//Set an ItemStack's display name
+	public ItemStack setName(String name) {
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.setDisplayName(name);
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+	//Set an ItemStack's lore with multiple lines separated by commas. Ex. "Line 1,Line 2,Line 3"
+	public ItemStack setLore(String lore) {
+		ItemMeta meta = item.getItemMeta();
+		
+		String[] lines = lore.split(",");
+		meta.setLore(Arrays.asList(lines));
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+	//Set ItemStack's owner if ItemStack is a Skull
+	public ItemStack setSkullOwner(String owner) {
+		if(item.getType() != Material.SKULL) //checks that item is a skull before changing owner
+			return item;
+		
+		SkullMeta skullMeta = (SkullMeta)item.getItemMeta();
+		skullMeta.setOwner(owner);
+		item.setItemMeta(skullMeta);
+		return item;
+	}
+	
+	public ItemStack getItem() {
+		return item;
+	}
+
+}
